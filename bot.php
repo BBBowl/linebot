9acc4da1cb89765870bd91d791b06204a01ceebd
KrisCode <kriscode@outlook.com>
2017-12-17 19:19:48 +0700

    seventh commit

--- bot.php
+++ bot.php
@@ -15,7 +15,7 @@ $_msg = $arrJson['events'][0]['message']['text'];
  
 $api_key="3rDixgr4hhgtGJCF2yfp6vag35Q10820";
 $url = 'https://api.mlab.com/api/1/databases/line-bbbot/collections/linebot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
+$json = file_get_contents('https://api.mlab.com/api/1/databases/line-bbbot/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
 $data = json_decode($json);
 $isData=sizeof($data);
  
