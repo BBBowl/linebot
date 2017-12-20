26b8a0543569493917e8f874ea836918c6506aa0
KrisCode <kriscode@outlook.com>
2017-12-20 19:12:11 +0700

    1st new commit

---
+++ bot/bot_conf.php
@@ -0,0 +1,12 @@
+<?php
+$ChannelSecret		= "
+1f21a8d60dbdb640249d04088175c17a
+";
+$ChannelAccessToken	= "
+OXrrG2XUc9kyILGUVDj1qUlP6P9NQ8/2dcsm/fHMXivqD5F4xTcWopoCgrI3BB72uiY3R+7EGn0xfE6Saow6c+pH6xUVy56ee1BXJqaqiKX7Qq1UxthEMWJWEcpzUTyVxs39xiMohlRqD8yhmaKT6QdB04t89/1O/w1cDnyilFU=
+";
+?>
+<?php
+	$LINEChannelSecret		= array("channelSecret" => trim($ChannelSecret));
+	$LINEChannelAccessToken	= trim($ChannelAccessToken);
+?>
---
+++ bot/bot_include.php
@@ -0,0 +1,5 @@
+<?php
+	@include($pathCurrent."vendor/autoload.php");
+	//@include($pathCurrent."bot/bot_namespace.php");
+	@include($pathCurrent."bot/bot_conf.php");
+?>
